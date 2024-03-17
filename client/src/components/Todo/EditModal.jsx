382ce82a63959a5cd001287c688be185fca29772
Danny Niedzwiedzki <danielniedzwiedzki.1@gmail.com>
2024-03-16 22:16:56 -0400

    refactor to edit modal modal

--- client/src/components/Todo/EditModal.jsx
+++ client/src/components/Todo/EditModal.jsx
@@ -1,7 +1,14 @@
 import 'react-datepicker/dist/react-datepicker.css';
 import { IoMdClose } from 'react-icons/io';
-import FormEditModal from './FormEditModal';
-import React, { useEffect } from 'react';
+// import FormEditModal from './FormEditModal';
+import React, { useEffect, useState } from 'react';
+import Select from './Select';
+import { parseISO } from 'date-fns';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useGlobalContext } from './todoContext';
+
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const EditModal = ({
   setShowEditModal,
@@ -22,11 +29,52 @@ const EditModal = ({
     };
   }, [showEditModal]);
 
+  const { updateItem, updateContent } = useGlobalContext();
+
+  const [currentItem, setCurrentItem] = useState({
+    currentTitle: updateItem.title,
+    description: updateItem.description,
+    isAddedToCal: updateItem.isAddedToCal,
+    updateCalEvent: true,
+  });
+
+  const [date, setDate] = useState(updateItem.dueDate);
+  const [category, setCategory] = useState(updateItem.category);
+
+  const updateCategory = (e) => {
+    setCategory(e);
+  };
+
+  const handleChange = (e) => {
+    setCurrentItem({ ...currentItem, [e.target.name]: e.target.value });
+  };
+
+  const handleCheckboxChange = (e) => {
+    setCurrentItem({ ...currentItem, updateCalEvent: e.target.checked });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const obj = {
+      _id: updateItem._id,
+      title: currentItem.currentTitle,
+      description: currentItem.description,
+      dueDate: date,
+      category: category,
+    };
+
+    currentItem.updateCalEvent ? (obj['calCode'] = updateItem.calCode) : null;
+    updateContent(obj, userSettings.sortBy);
+    updateItemsAfterEditTodo(); //trigger update to items sort by
+    setShowEditModal(false);
+  };
+
   return (
     <div className="relative z-10" role="dialog" aria-modal="true">
       <div className="fixed inset-0 bg-gray-500 bg-opacity-40 transition-opacity z-10"></div>
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex items-center justify-center p-4 text-center sm:items-center sm:p-0 h-full">
+        <div className="flex items-center justify-center p-4 text-center sm:items-center sm:p-0 h-full ">
           <div className="transform  bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-4xl self-center overflow-visible w-full md:w-[32rem]">
             <div className="font-bold text-4xl p-2 pt-0 pb-0 flex justify-between items-center bg-gray-100">
               <p className="text-sm ml-1 pt-1 text-gray-600 tracking-widest">
@@ -41,14 +89,102 @@ const EditModal = ({
               </button>
             </div>
 
-            <div>
-              <FormEditModal
-                updateItemsAfterEditTodo={updateItemsAfterEditTodo}
-                userSettings={userSettings}
-                setUserSettings={setUserSettings}
-                setShowEditModal={setShowEditModal}
-              />
+            <div className="bg-white px-4 pb-4 !pt-0 sm:p-6 sm:pb-4 h-fit ">
+              <div className="">
+                <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                  <div className="mt-2">
+                    <div className="align-center">
+                      <form onSubmit={handleSubmit}>
+                        {/* title */}
+                        <div className="mb-4">
+                          <label className="block text-gray-700 text-sm font-bold mb-2">
+                            {/* Title */}
+                          </label>
+                          <input
+                            className="pt-3 text-gray-600 text-md font-semibold pb-2 w-full border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+                            id="currentTitle"
+                            type="text"
+                            name="currentTitle"
+                            placeholder="Title"
+                            required
+                            value={currentItem.currentTitle}
+                            onChange={(e) => handleChange(e)}
+                          />
+                        </div>
+                        {/* description */}
+                        <div className="mb-4">
+                          <label className="block text-gray-700 text-sm font-bold mb-2">
+                            {/* Description */}
+                          </label>
+                          <textarea
+                            className="pt-3 text-gray-600 text-md font-semibold w-full border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500 h-32"
+                            id="description"
+                            type="textarea"
+                            name="description"
+                            placeholder="Description"
+                            required
+                            value={currentItem.description}
+                            onChange={handleChange}
+                          />
+                        </div>
+                        {/* time */}
+                        <div className="mb-4 flex flex-col justify-start">
+                          <label className="block text-gray-700 text-sm font-bold mb-2 ">
+                            {/* Due */}
+                          </label>
+                          <DatePicker
+                            showTimeSelect
+                            selected={parseISO(date)}
+                            onChange={(date) => setDate(date.toISOString())}
+                            dateFormat="MMMM d, yyyy h:mmaa"
+                            className="pt-3 text-gray-600 text-sm font-semibold pb-2 !w-60 border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+                          />
+
+                          <Select
+                            textPrompt={'Select'}
+                            className="relative mt-3 p-3 text-sm font-semibold cursor-pointer w-32  bg-white border-solid border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+                            userSettings={userSettings}
+                            setUserSettings={setUserSettings}
+                            updatable={false}
+                            updateCategory={updateCategory}
+                            category={category}
+                          />
+                        </div>
+
+                        {updateItem.calCode ? (
+                          <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">
+                              Update Calendar event?
+                            </label>
+                            <input
+                              className="bg-slate-200"
+                              type="checkbox"
+                              checked={currentItem.updateCalEvent}
+                              onChange={handleCheckboxChange}
+                            />
+                          </div>
+                        ) : null}
+
+                        <div className="flex justify-center">
+                          <button
+                            className="block w-96 text-white h-12 text-center rounded-md bg-black px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset hover:opacity-80 sm:mt-0 tracking-widest"
+                            type="submit"
+                          >
+                            {!updateItem.isCountDown ? (
+                              <p>Update Todo </p>
+                            ) : (
+                              <p>Update Countdown </p>
+                            )}
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+
+            {/*  */}
           </div>
         </div>
       </div>
--- client/src/components/Todo/FormEditModal.jsx
+++ client/src/components/Todo/FormEditModal.jsx
@@ -1,155 +1,155 @@
-import { useState } from 'react';
-import { useGlobalContext } from './todoContext';
+// import { useState } from 'react';
+// import { useGlobalContext } from './todoContext';
 
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
 
-import { parseISO } from 'date-fns';
-import Select from './Select';
+// import { parseISO } from 'date-fns';
+// import Select from './Select';
 
-const FormEditModal = ({
-  updateItemsAfterEditTodo,
-  userSettings,
-  setUserSettings,
-  setShowEditModal,
-}) => {
-  const { updateItem, updateContent } = useGlobalContext();
+// const FormEditModal = ({
+//   updateItemsAfterEditTodo,
+//   userSettings,
+//   setUserSettings,
+//   setShowEditModal,
+// }) => {
+//   const { updateItem, updateContent } = useGlobalContext();
 
-  const [currentItem, setCurrentItem] = useState({
-    currentTitle: updateItem.title,
-    description: updateItem.description,
-    isAddedToCal: updateItem.isAddedToCal,
-    updateCalEvent: true,
-  });
+//   const [currentItem, setCurrentItem] = useState({
+//     currentTitle: updateItem.title,
+//     description: updateItem.description,
+//     isAddedToCal: updateItem.isAddedToCal,
+//     updateCalEvent: true,
+//   });
 
-  const [date, setDate] = useState(updateItem.dueDate);
-  const [category, setCategory] = useState(updateItem.category);
+//   const [date, setDate] = useState(updateItem.dueDate);
+//   const [category, setCategory] = useState(updateItem.category);
 
-  const updateCategory = (e) => {
-    setCategory(e);
-  };
+//   const updateCategory = (e) => {
+//     setCategory(e);
+//   };
 
-  const handleChange = (e) => {
-    setCurrentItem({ ...currentItem, [e.target.name]: e.target.value });
-  };
+//   const handleChange = (e) => {
+//     setCurrentItem({ ...currentItem, [e.target.name]: e.target.value });
+//   };
 
-  const handleCheckboxChange = (e) => {
-    setCurrentItem({ ...currentItem, updateCalEvent: e.target.checked });
-  };
+//   const handleCheckboxChange = (e) => {
+//     setCurrentItem({ ...currentItem, updateCalEvent: e.target.checked });
+//   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
 
-    const obj = {
-      _id: updateItem._id,
-      title: currentItem.currentTitle,
-      description: currentItem.description,
-      dueDate: date,
-      category: category,
-    };
+//     const obj = {
+//       _id: updateItem._id,
+//       title: currentItem.currentTitle,
+//       description: currentItem.description,
+//       dueDate: date,
+//       category: category,
+//     };
 
-    currentItem.updateCalEvent ? (obj['calCode'] = updateItem.calCode) : null;
-    updateContent(obj, userSettings.sortBy);
-    updateItemsAfterEditTodo(); //trigger update to items sort by
-    setShowEditModal(false);
-  };
+//     currentItem.updateCalEvent ? (obj['calCode'] = updateItem.calCode) : null;
+//     updateContent(obj, userSettings.sortBy);
+//     updateItemsAfterEditTodo(); //trigger update to items sort by
+//     setShowEditModal(false);
+//   };
 
-  return (
-    <div className="bg-white px-4 pb-4 !pt-0 sm:p-6 sm:pb-4 h-fit bg-red-300">
-      <div className="modalClass">
-        <div className="mt-3 text-center sm:mt-0 sm:text-left">
-          <div className="mt-2">
-            <div className="align-center">
-              <form onSubmit={handleSubmit}>
-                {/* title */}
-                <div className="mb-4">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">
-                    {/* Title */}
-                  </label>
-                  <input
-                    className="pt-3 text-gray-600 text-md font-semibold pb-2 w-full border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
-                    id="currentTitle"
-                    type="text"
-                    name="currentTitle"
-                    placeholder="Title"
-                    required
-                    value={currentItem.currentTitle}
-                    onChange={(e) => handleChange(e)}
-                  />
-                </div>
-                {/* description */}
-                <div className="mb-4">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">
-                    {/* Description */}
-                  </label>
-                  <textarea
-                    className="pt-3 text-gray-600 text-md font-semibold w-full border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500 h-32"
-                    id="description"
-                    type="textarea"
-                    name="description"
-                    placeholder="Description"
-                    required
-                    value={currentItem.description}
-                    onChange={handleChange}
-                  />
-                </div>
-                {/* time */}
-                <div className="mb-4 flex flex-col justify-start">
-                  <label className="block text-gray-700 text-sm font-bold mb-2 ">
-                    {/* Due */}
-                  </label>
-                  <DatePicker
-                    showTimeSelect
-                    selected={parseISO(date)}
-                    onChange={(date) => setDate(date.toISOString())}
-                    dateFormat="MMMM d, yyyy h:mmaa"
-                    className="pt-3 text-gray-600 text-sm font-semibold pb-2 !w-60 border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
-                  />
+//   return (
+//     <div className="bg-white px-4 pb-4 !pt-0 sm:p-6 sm:pb-4 h-fit bg-red-300">
+//       <div className="modalClass">
+//         <div className="mt-3 text-center sm:mt-0 sm:text-left">
+//           <div className="mt-2">
+//             <div className="align-center">
+//               <form onSubmit={handleSubmit}>
+//                 {/* title */}
+//                 <div className="mb-4">
+//                   <label className="block text-gray-700 text-sm font-bold mb-2">
+//                     {/* Title */}
+//                   </label>
+//                   <input
+//                     className="pt-3 text-gray-600 text-md font-semibold pb-2 w-full border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+//                     id="currentTitle"
+//                     type="text"
+//                     name="currentTitle"
+//                     placeholder="Title"
+//                     required
+//                     value={currentItem.currentTitle}
+//                     onChange={(e) => handleChange(e)}
+//                   />
+//                 </div>
+//                 {/* description */}
+//                 <div className="mb-4">
+//                   <label className="block text-gray-700 text-sm font-bold mb-2">
+//                     {/* Description */}
+//                   </label>
+//                   <textarea
+//                     className="pt-3 text-gray-600 text-md font-semibold w-full border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500 h-32"
+//                     id="description"
+//                     type="textarea"
+//                     name="description"
+//                     placeholder="Description"
+//                     required
+//                     value={currentItem.description}
+//                     onChange={handleChange}
+//                   />
+//                 </div>
+//                 {/* time */}
+//                 <div className="mb-4 flex flex-col justify-start">
+//                   <label className="block text-gray-700 text-sm font-bold mb-2 ">
+//                     {/* Due */}
+//                   </label>
+//                   <DatePicker
+//                     showTimeSelect
+//                     selected={parseISO(date)}
+//                     onChange={(date) => setDate(date.toISOString())}
+//                     dateFormat="MMMM d, yyyy h:mmaa"
+//                     className="pt-3 text-gray-600 text-sm font-semibold pb-2 !w-60 border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+//                   />
 
-                  <Select
-                    textPrompt={'Select'}
-                    className="relative mt-3 p-3 text-sm font-semibold cursor-pointer w-32  bg-white border-solid border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
-                    userSettings={userSettings}
-                    setUserSettings={setUserSettings}
-                    updatable={false}
-                    updateCategory={updateCategory}
-                    category={category}
-                  />
-                </div>
+//                   <Select
+//                     textPrompt={'Select'}
+//                     className="relative mt-3 p-3 text-sm font-semibold cursor-pointer w-32  bg-white border-solid border-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+//                     userSettings={userSettings}
+//                     setUserSettings={setUserSettings}
+//                     updatable={false}
+//                     updateCategory={updateCategory}
+//                     category={category}
+//                   />
+//                 </div>
 
-                {updateItem.calCode ? (
-                  <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                      Update Calendar event?
-                    </label>
-                    <input
-                      className="bg-slate-200"
-                      type="checkbox"
-                      checked={currentItem.updateCalEvent}
-                      onChange={handleCheckboxChange}
-                    />
-                  </div>
-                ) : null}
+//                 {updateItem.calCode ? (
+//                   <div className="mb-4">
+//                     <label className="block text-gray-700 text-sm font-bold mb-2">
+//                       Update Calendar event?
+//                     </label>
+//                     <input
+//                       className="bg-slate-200"
+//                       type="checkbox"
+//                       checked={currentItem.updateCalEvent}
+//                       onChange={handleCheckboxChange}
+//                     />
+//                   </div>
+//                 ) : null}
 
-                <div className="flex justify-center">
-                  <button
-                    className="block w-96 text-white h-12 text-center rounded-md bg-black px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset hover:opacity-80 sm:mt-0 tracking-widest"
-                    type="submit"
-                  >
-                    {!updateItem.isCountDown ? (
-                      <p>Update Todo </p>
-                    ) : (
-                      <p>Update Countdown </p>
-                    )}
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+//                 <div className="flex justify-center">
+//                   <button
+//                     className="block w-96 text-white h-12 text-center rounded-md bg-black px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset hover:opacity-80 sm:mt-0 tracking-widest"
+//                     type="submit"
+//                   >
+//                     {!updateItem.isCountDown ? (
+//                       <p>Update Todo </p>
+//                     ) : (
+//                       <p>Update Countdown </p>
+//                     )}
+//                   </button>
+//                 </div>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default FormEditModal;
+// export default FormEditModal;
